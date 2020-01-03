c6dae3459f889dac38edb6c01c4c9bd907167bbe
sundegang <sundegang@git.com.cn>
2020-01-03 09:28:47 +0800

    提交测试

--- msdp-run/src/main/java/cn/com/git/msdp/App.java
+++ msdp-run/src/main/java/cn/com/git/msdp/App.java
@@ -4,10 +4,8 @@ package cn.com.git.msdp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
