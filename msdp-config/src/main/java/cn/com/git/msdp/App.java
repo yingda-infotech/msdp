b09e28341440514cd14bdd92b783630ee2ac5576
chenyouguang <you@example.com>
2020-01-03 09:16:53 +0800

    首次导入

---
+++ msdp-config/src/main/java/cn/com/git/msdp/App.java
@@ -0,0 +1,13 @@
+package cn.com.git.msdp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
